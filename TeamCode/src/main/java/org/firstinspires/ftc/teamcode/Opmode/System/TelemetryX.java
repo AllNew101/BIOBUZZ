6a432828403c65b1341e0634a6130048bf0237bc
Natphawarun Atchamanmitr <allnew101.team24070@gmail.com>
2026-06-26 00:15:02 +0700

    Test01

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/System/TelemetryX.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Opmode.System;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+
+
+public class  TelemetryX {
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    Telemetry dashboardTelemetry = dashboard.getTelemetry();
+    Telemetry telemetry;
+
+    public void init(Telemetry t){
+        telemetry = t;
+    }
+    public void addData(String head, Object child, int key){
+        switch (key) {
+            case 0:
+                dashboardTelemetry.addData(head, child);
+                break;
+            case 1:
+                telemetry.addData(head, child);
+                break;
+            case 2:
+                dashboardTelemetry.addData(head, child);
+                telemetry.addData(head, child);
+                break;
+        }
+    }
+
+    public void update(){
+        dashboardTelemetry.update();
+        telemetry.update();
+    }
+    public void clear(){
+        dashboardTelemetry.clear();
+        telemetry.clear();
+    }
+}
+
+
